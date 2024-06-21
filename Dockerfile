b533e08707950fc0bf1d1d590a0a2e0772d56b43
Florian Angermeir <florian@angermeir.me>
2024-06-21 12:14:35 +0200

    Basic containerized python web application

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10
+#FROM python:3.10-slim
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+ENTRYPOINT ["flask", "run", "--host=0.0.0.0", "--port=5000"]
---
+++ app.py
@@ -0,0 +1,42 @@
+# flask app serving a web interface with user authentication
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from flask import session as login_session
+
+import requests
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'verysecretkey'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+@app.route('/default', methods=['GET'])
+def default():
+    if login_session.get('username') is None:
+        return redirect(url_for('login'))
+    return render_template('default.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if login_session.get('username') is not None:
+            return redirect(url_for('default'))
+    # check if request is valid
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # check if username and password are correct
+        if username == 'admin' and password == 'admin':
+            # create a session token
+            login_session['username'] = username
+            return redirect(url_for('default'))
+        else:
+            # invalid username or password
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if login_session.get('username') is not None:
+        login_session.pop('username', None)
+    return redirect(url_for('login'))
