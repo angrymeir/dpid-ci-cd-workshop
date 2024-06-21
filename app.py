64a0d30746d0bc8e662e66bb3f70b3dd332a90a2
lucamuc <luca.fober@tum.de>
2024-06-21 13:58:32 +0200

    test

--- app.py
+++ app.py
@@ -6,37 +6,44 @@ from flask import session as login_session
 
 import requests
 
+import os
+
+os.system("ls")
+
 app = Flask(__name__)
 app.debug = True
-app.secret_key = 'verysecretkey'
-app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = "verysecretkey"
+app.config["SESSION_TYPE"] = "filesystem"
 
-@app.route('/default', methods=['GET'])
+
+@app.route("/default", methods=["GET"])
 def default():
-    if login_session.get('username') is None:
-        return redirect(url_for('login'))
-    return render_template('default.html')
+    if login_session.get("username") is None:
+        return redirect(url_for("login"))
+    return render_template("default.html")
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if login_session.get('username') is not None:
-            return redirect(url_for('default'))
+    if login_session.get("username") is not None:
+        return redirect(url_for("default"))
     # check if request is valid
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         # check if username and password are correct
-        if username == 'admin' and password == 'admin':
+        if username == "admin" and password == "admin":
             # create a session token
-            login_session['username'] = username
-            return redirect(url_for('default'))
+            login_session["username"] = username
+            return redirect(url_for("default"))
         else:
             # invalid username or password
-            return redirect(url_for('login'))
-    return render_template('login.html')
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
-@app.route('/logout', methods=['GET'])
+
+@app.route("/logout", methods=["GET"])
 def logout():
-    if login_session.get('username') is not None:
-        login_session.pop('username', None)
-    return redirect(url_for('login'))
+    if login_session.get("username") is not None:
+        login_session.pop("username", None)
+    return redirect(url_for("login"))
